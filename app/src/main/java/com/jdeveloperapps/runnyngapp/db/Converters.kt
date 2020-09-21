4bd56eeb300a4d3827e2285be7c9b6980a2fb2bb
Jvitaliy82 <jvit82@gmail.com>
2020-09-22 00:23:06 +0700

    add RoomDatabase

---
+++ app/src/main/java/com/jdeveloperapps/runnyngapp/db/Converters.kt
@@ -0,0 +1,22 @@
+package com.jdeveloperapps.runnyngapp.db
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    @TypeConverter
+    fun toBitmap(byte: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(byte, 0, byte.size)
+    }
+
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val  outputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+
+}
