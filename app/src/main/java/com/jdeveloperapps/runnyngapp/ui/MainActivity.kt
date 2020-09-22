29ddd7f8d1203953626185c5578d21bccce3ca96
Jvitaliy82 <jvit82@gmail.com>
2020-09-22 23:38:20 +0700

    add notification

--- app/src/main/java/com/jdeveloperapps/runnyngapp/ui/MainActivity.kt
+++ app/src/main/java/com/jdeveloperapps/runnyngapp/ui/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.jdeveloperapps.runnyngapp.ui
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.jdeveloperapps.runnyngapp.R
-import com.jdeveloperapps.runnyngapp.db.RunDAO
+import com.jdeveloperapps.runnyngapp.other.Constans.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -31,5 +32,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
